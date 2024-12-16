63694f579441585a3aa31e30fd9eaa9572883e2c
arafat1023 <arafathossain847@gmail.com>
2024-12-16 18:39:22 +0600

    Designing and Validating the Form

--- blogproject/blog/forms.py
+++ blogproject/blog/forms.py
@@ -31,3 +31,9 @@ class ContactForm(forms.Form):
     name = forms.CharField(max_length=100, label="Your Name")
     email = forms.EmailField(label="Your Email")
     message = forms.CharField(widget=forms.Textarea, label="Your Message")
+
+    def clean_message(self):
+        message = self.cleaned_data['message']
+        if len(message) < 10:
+            raise forms.ValidationError("Message must be at least 10 characters long.")
+        return message
